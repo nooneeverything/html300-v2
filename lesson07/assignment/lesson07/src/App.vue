1c632690f016001a0b88931edd0ad3ae028d4b58
Noah Tesema <noahtesema@gmail.com>
2023-06-02 22:12:56 -0700

    Lesson 07 Assignment

---
+++ lesson07/assignment/lesson07/src/App.vue
@@ -0,0 +1,62 @@
+<template>
+  <div id="app">
+    <h1 class="text-center text-capitalize m-5"> Law Offices of Yema Measho </h1>
+      <b-nav class="navbar navbar-expand-lg navbar-light" style="background-color: #e3f2fd;">
+                <div class="container-fluid">
+                  <a class="navbar-brand" href="#">Navbar</a>
+                  <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                  <span class="navbar-toggler-icon"></span>
+                  </button>
+                  <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav p-4 align-middle">
+                      <li class="nav-item">
+                        <!-- Tooltip-->
+                        <a class="nav-link active" aria-current="page" href="/" data-toggle="tooltip" data-placement="bottom" title="Back to Homepage">Home</a>
+                      </li>
+                      <!-- Tooltip -->
+                      <li class="nav-item">
+                        <a class="nav-link" href="team" data-toggle="tooltip" data-placement="bottom" title="See Our Team">Team</a>
+                      </li>
+                      <!-- Tooltip -->
+                      <li class="nav-item">
+                        <a class="nav-link" href="aboutus" data-toggle="tooltip" data-placement="bottom" title="Learn More About Our Firm"f>About Us</a>
+                      </li>
+                      <!-- Tooltip -->
+                      <li class="nav-item">
+                        <a class="nav-link" href="contact"  data-toggle="tooltip" data-placement="bottom" title="Contact Information">Contact</a>
+                      </li>
+                      <!-- Tooltip -->
+                      <li class="nav-item">
+                        <a class="nav-link" href="#" data-toggle="tooltip" data-placement="bottom" title="Unavaiable">Location</a>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+              </b-nav>
+              
+    <router-view> </router-view>
+  </div>
+</template>
+
+<style lang="scss">
+#app {
+  font-family: "Times New Roman", Times, serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+/*Style NavBar*/
+.routerlink{
+  color: rgb(255, 255, 255)
+}
+
+.dropdown{
+  color: rgb(0, 0, 0)
+}
+
+
+
+</style>
---
+++ lesson07/assignment/lesson07/src/views/Contact.vue
@@ -0,0 +1,52 @@
+<template>
+<!-- V-If Statment Used to Change Content-->
+
+
+    <b-container>
+      <b-row>
+
+        <b-col cols="6">
+          <h3 class="text-center pb-3 mt-5">{{header1}}</h3>
+          <p v-if="contactinfo.length >= text">{{contactinfo}}</p>
+        
+        </b-col>
+        <b-col cols="6">
+          <h3 class="text-center mb-4 mt-5">{{header2}}</h3>
+          <p v-if="contactinfo.length >= text">{{contactinfo}}</p>
+        </b-col>
+      </b-row>
+    
+
+      <b-row>
+        <b-col cols="6">
+          <h3 class="text-center pb-3 mt-5"> {{header3}} </h3>
+          <p v-if="contactinfo.length >= text">{{contactinfo}}</p>
+        </b-col>
+      
+    
+      
+        <b-col cols="6">
+          <h3 class="text-center pb-3 mt-5">{{header4}}</h3>
+          <p v-if="contactinfo.length >= text">{{contactinfo}}</p>
+        </b-col>
+      </b-row>
+    </b-container>
+    </template>
+    
+    <script>
+    
+
+      export default{
+        name: 'app',
+        data(){
+          return{
+            contactinfo: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.Aenean tempus ligula ac elit porta, nec ornare mauris bibendum. Donec vitae justo tristique, ornare diam in, imperdiet orci.Curabitur sit amet eros semper mauris ultricies consequat ut et ante.Morbi pellentesque dolor ac orci ultrices, eu sagittis eros dignissim.",
+            text: 48,
+            header1: 'Email',
+            header2: 'Phone',
+            header3: 'Address',
+            header4: 'Messenger Pigeon'
+        }
+      }
+      }
+    </script>
