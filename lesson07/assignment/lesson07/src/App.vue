6bda4ccb00015096336777e8e6786609f894ccfa
Noah Tesema <noahtesema@gmail.com>
2023-06-06 14:18:35 -0700

    Lesson 7 Nav Bar Fix

--- lesson07/assignment/lesson07/src/App.vue
+++ lesson07/assignment/lesson07/src/App.vue
@@ -1,40 +1,49 @@
 <template>
   <div id="app">
+
+  <div>
+  <b-navbar toggleable="lg" type="light" variant="info">  
     <h1 class="text-center text-capitalize m-5"> Law Offices of Yema Measho </h1>
-      <b-nav class="navbar navbar-expand-lg navbar-light" style="background-color: #e3f2fd;">
-                <div class="container-fluid">
-                  <a class="navbar-brand" href="#">Navbar</a>
-                  <b-button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                  <span class="navbar-toggler-icon"></span>
-                  </b-button>
-                  <div class="collapse navbar-collapse" id="navbarNav">
-                    <ul class="navbar-nav p-4 align-middle">
-                      <li class="nav-item">
-                        <!-- Tooltip-->
-                        <router-link class="nav-link" to="/" data-bs-toggle="tooltip" data-bs-placement="bottom" title="Back to Homepage">Home</router-link>
-                      </li>
-                      <!-- Tooltip -->
-                      <li class="nav-item">
-                        <router-link class="nav-link" to="/team" data-bs-toggle="tooltip" data-bs-placement="bottom" title="See Our Team">Team</router-link>
-                      </li>
-                      <!-- Tooltip -->
-                      <li class="nav-item">
-                        <router-link class="nav-link" to="/aboutus" data-bs-toggle="tooltip" data-bs-placement="bottom" title="Learn More About Our Firm">About Us</router-link>
-                      </li>
-                      <!-- Tooltip -->
-                      <li class="nav-item">
-                        <router-link class="nav-link" to="/contact" data-bs-toggle="tooltip" data-bs-placement="bottom" title="Contact Informattion"> Contact</router-link>
-                      </li>
-                      <!-- Tooltip -->  
-                      <li class="nav-item">
-                        <router-link class="nav-link" to="/#" data-bs-toggle="tooltip" data-bs-placement="bottom" title="Unavaiable">Locaion</router-link>
-                      </li>
-                    </ul>
-                  </div>
-                </div>
-        </b-nav>
-              
-   <router-view> </router-view>
+    <b-navbar-brand><router-link class="nav-link" to="/" data-bs-toggle="tooltip" data-bs-placement="bottom" title="Back to Homepage">Home</router-link></b-navbar-brand>
+    <b-navbar-brand><router-link class="nav-link" to="/team" data-bs-toggle="tooltip" data-bs-placement="bottom" title="See Our Team">Team</router-link></b-navbar-brand>
+    <b-navbar-brand><router-link class="nav-link" to="/aboutus" data-bs-toggle="tooltip" data-bs-placement="bottom" title="Learn More About Our Firm">About Us</router-link></b-navbar-brand>
+    <b-navbar-brand><router-link class="nav-link" to="/contact" data-bs-toggle="tooltip" data-bs-placement="bottom" title="Contact Informattion"> Contact</router-link></b-navbar-brand>
+
+    <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+    <b-collapse id="nav-collapse" is-nav>
+      <!-- <b-navbar-nav>
+        <b-nav-item href="#">Link</b-nav-item>
+        <b-nav-item href="#" disabled>Disabled</b-nav-item>
+      </b-navbar-nav> -->
+
+      <!-- Right aligned nav items -->
+      <b-navbar-nav class="ml-auto">  
+        <!--<b-nav-form>
+          <b-form-input size="sm" class="mr-sm-2" placeholder="Search"></b-form-input>
+          <b-button size="sm" class="my-2 my-sm-0" type="submit">Search</b-button>
+        </b-nav-form> -->
+
+        <b-nav-item-dropdown text="Lang" right>
+          <b-dropdown-item href="#">EN</b-dropdown-item>
+          <b-dropdown-item href="#">ES</b-dropdown-item>
+          <b-dropdown-item href="#">RU</b-dropdown-item>
+          <b-dropdown-item href="#">FA</b-dropdown-item>
+        </b-nav-item-dropdown>
+
+        <b-nav-item-dropdown right>
+          <!-- Using 'button-content' slot -->
+          <template #button-content>
+            <em>User</em>
+          </template>
+          <b-dropdown-item href="#">Profile</b-dropdown-item>
+          <b-dropdown-item href="#">Sign Out</b-dropdown-item>
+        </b-nav-item-dropdown>
+      </b-navbar-nav>
+    </b-collapse>
+  </b-navbar>
+</div>
+  <router-view> </router-view>
   </div>
 </template>
 
